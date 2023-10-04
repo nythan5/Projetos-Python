4908ace374f13c7e30f5d73ae0a55a39cc0fd115
Perkles <g.n.d2014@gmail.com>
2023-10-03 21:05:37 -0300

    Entrando no site e tirando o involucro

--- Analise_Combustivel/navegador.py
+++ Analise_Combustivel/navegador.py
@@ -1,9 +1,12 @@
 import time
 
 import selenium.common.exceptions
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 
 class BaixarExcel:
@@ -11,14 +14,28 @@ class BaixarExcel:
         # Service Navegador
         self.service = Service(ChromeDriverManager().install())
         self.navegador = None
-        self.link = "https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/levantamento-de-precos-de-combustiveis-ultimas-semanas-pesquisadas"
+        self.link = 'https://www.gov.br/anp/pt-br/assuntos/precos-e-defesa-da-concorrencia/precos/levantamento-de-precos-de-combustiveis-ultimas-semanas-pesquisadas'
+
+
+
 
     def abri_navegador(self):
         self.navegador = webdriver.Chrome(service=self.service)
         link_site = self.link
         self.navegador.get(link_site)
 
-        # Texto que ele vai procurar para clicar no link
+        time.sleep(2)
+
+
+
+
+        # Ele sai do invólucro criado para forçar um login
+        self.navegador.find_element('xpath', '/html/body').click()
+        time.sleep(1)
+        #self.navegador.find_element('xpath', '//*[@id="parent-fieldname-text"]/ul[1]/li[2]/a')
+
+        self.navegador.find_element('xpath', '//*[@id="parent-fieldname-text"]/ul[1]/li[2]/a')
+
 
 
         time.sleep(90000)
