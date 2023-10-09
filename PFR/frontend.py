a8519a0ab414fa56f01372e52679938add338ca1
Perkles <g.n.d2014@gmail.com>
2023-10-08 22:07:51 -0300

    Projeto Vitalab Pythonando

--- PFR/frontend.py
+++ PFR/frontend.py
@@ -3,62 +3,50 @@ import tkinter as tk
 from tkinter import ttk
 from backend import AutomacaoPfr
 import sys
-from PIL import Image
-from PIL import ImageTk
 
 
 class InterfaceGrafica:
-    def __init__(self, janela, lista_pfr_preenchidas):
+    def __init__(self, janela,lista_pfr_preenchidas):
         self.janela = janela
         self.janela.title("Confirmação de PFR's")
-        self.janela.geometry("490x130")
-
-        # Configurar ícone da aplicação
-        self.icon = ImageTk.PhotoImage(file="../PFR/icon/list_notes_930.ico")
-        self.janela.iconphoto(True, self.icon)
+        self.janela.geometry("450x130")
 
         # Status da aplicação
         self.event = threading.Event()
         self.lista_pfr_preenchidas = lista_pfr_preenchidas
-        self.total_linhas = app.carregar_planilha()[1]
+
 
 
         # Botão "Iniciar"
-        self.botao_iniciar = tk.Button(self.janela, text="Iniciar Processo", command=self.iniciar, width=15, height=3)
-        self.botao_iniciar.pack(side=tk.LEFT, padx=5, pady=4)
+        self.botao_iniciar = tk.Button(self.janela, text="Iniciar", command=self.iniciar,width=15, height=3)
+        self.botao_iniciar.pack(side=tk.LEFT,padx=5, pady=4)
 
         # Botão "Finalizar"
-        self.botao_finalizar = tk.Button(self.janela, text="Parar Processo", command=self.finalizar, width=15, height=3)
-        self.botao_finalizar.pack(side=tk.RIGHT, padx=5, pady=4)
+        self.botao_finalizar = tk.Button(self.janela, text="Finalizar", command=self.finalizar,width=15, height=3)
+        self.botao_finalizar.pack(side=tk.RIGHT,padx=5, pady=4)
 
         # Rótulo acima da Listbox
-        self.label_lista = tk.Label(self.janela,
-                                    text=f"Lista de PFR's confirmada: {len(self.lista_pfr_preenchidas)} "
-                                         f"de {self.total_linhas}")
+        self.label_lista = tk.Label(self.janela, text="Lista de PFR's confirmadas!")
         self.label_lista.pack(side=tk.TOP, padx=5, pady=2)
 
         # Lista de PFRs preenchidas (widget)
         self.lista_pfr_widget = tk.Listbox(self.janela)
-        self.lista_pfr_widget.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+        self.lista_pfr_widget.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-        # Callback que atualiza a listagem na Interface Grafica
-        app.set_callback(self.atualizar_lista)
 
-    def atualizar_lista(self, pfr):
-        try:
-            self.lista_pfr_widget.insert(tk.END, pfr)
-            self.atualizar_label()
-        except TypeError:
-            print("Não foi possivel atualizar a lista")
-
-    def atualizar_label(self):
-        total_elementos = len(self.lista_pfr_widget.get(0, tk.END))
-        self.label_lista.config(text=f"Lista de PFR's confirmada: {len(self.lista_pfr_preenchidas)} "
-                                     f"de {self.total_linhas}")
+    def atualizar_lista(self):
+        for item in self.lista_pfr_preenchidas:
+            self.lista_pfr_widget.insert(tk.END, item)
 
     def codigo_a_executar(self):
-        app.iniciar_navegador()
+        self.atualizar_lista()
+        #app.iniciar_navegador()
         app.iniciar_automacao()
+        self.atualizar_lista()
+
+
+
+
 
     def iniciar(self):
         # Ação a ser realizada ao clicar no botão "Iniciar"
@@ -72,18 +60,21 @@ class InterfaceGrafica:
             self.status_aplicacao = False
             print("Aplicação Encerrada")
 
+
+
     def finalizar(self):
         # Ação a ser realizada ao clicar no botão "Finalizar"
         print("Finalizando...")
         app.fechar_navegador()
         self.event.set()
-        # self.janela.destroy()
-        # sys.exit()
+        self.janela.destroy()
+        sys.exit()
 
 
 if __name__ == "__main__":
     janela_principal = tk.Tk()
     app = AutomacaoPfr()
-    lista_pfr_preenchidas = app.lista_pfr_preenchidas
+    lista_pfr_preenchidas = app.lista_pfr_naorealizadas
+    print(lista_pfr_preenchidas)
     tela = InterfaceGrafica(janela_principal, lista_pfr_preenchidas)
     janela_principal.mainloop()
